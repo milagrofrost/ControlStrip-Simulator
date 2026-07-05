fc6ef4e80d39efc9d27fb8fc490a095b77dce9f1
milagrofrost <milagrofrost@users.noreply.github.com>
2026-07-05 09:41:31 -0400

    Fix packaged asset loading for deb build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
+  build: {
+    assetsInlineLimit: 0
+  },
   server: {
     port: 5173,
     strictPort: true,
